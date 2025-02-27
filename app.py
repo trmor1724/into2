4ceb413491c9c46b70303672ead6b3f346f882e6
tremor1724 <148923646+trmor1724@users.noreply.github.com>
2025-02-27 07:13:14 -0500

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
-
+from PIL import Image
 
 st.title("Mi primera app")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("facilmente puedo realizar backend y frontend:")
+image = Image.open("gato.jpg")
