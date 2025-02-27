a1fb2393e007adca7e34252b7c3ae2ec891ed64d
tremor1724 <148923646+trmor1724@users.noreply.github.com>
2025-02-27 07:15:49 -0500

    app.py

--- app.py
+++ app.py
@@ -6,3 +6,5 @@ st.title("Mi primera app")
 st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
 st.write("facilmente puedo realizar backend y frontend:")
 image = Image.open("gato.jpg")
+
+st.image(image, caption="Interfaces multimodales")
