0f61861c1b5d68115639f4ad40f6f6561e37e32f
tremor1724 <148923646+trmor1724@users.noreply.github.com>
2025-02-27 07:37:05 -0500

    app.py

--- app.py
+++ app.py
@@ -16,3 +16,23 @@ st.write("Texto ingresado:", texto)
 st.subheader("Ahora usemos 2 columnas")
 
 col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("Las interfaces multimodales mejoran la experiencia del usuario")
+  resp = st.checkbox("Estoy de acuerdo")
+  if resp:
+    st.write("Correcto")
+
+with col2:
+  st.subheader("Esta es la segunda columna")
+  modo = st.radio("Que modalidad es la principal en tu interfaz", ("Visual","Auditiva", "Tactil")
+  if modo == "Visual"
+    st.write("La vista es fundamental para tu interfaz")
+  if modo == "Auditiva"
+    st.write("La audicion es fundamental para tu interfaz")
+  if modo == "Visual"
+    st.write("El tacto es fundamental para tu interfaz")
+                  
+  
+  
